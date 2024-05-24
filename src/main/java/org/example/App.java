0b07694a8887d2ee12f2b38ded382594f8a51304
Marat Gafiev <gmkorg109@gmail.com>
2024-05-24 19:39:27 +0300

    init commit Greetings current Time

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.time.LocalTime;
+
 /**
  * Hello Esmira!
  *
@@ -9,5 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Esmira!" );
+
+        System.out.println(LocalTime.now());
     }
 }
