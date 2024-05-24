43fc464146099166a118725e6e6f369609b30f80
Marat Gafiev <gmkorg109@gmail.com>
2024-05-24 19:28:23 +0300

    init commit Greetings

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,13 @@
 package org.example;
 
 /**
- * Hello world!
+ * Hello Esmira!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Esmira!" );
     }
 }
